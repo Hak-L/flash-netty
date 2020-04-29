a869c6141d1bb575a25d6d4bdac8602a0c84470e
json_shen@skytech.cn <json_shen@skytech.cn>
2020-04-29 16:57:35 +0800

    增加一些代码注释，方便理解

--- src/main/java/the/flash/NettyServer.java
+++ src/main/java/the/flash/NettyServer.java
@@ -13,6 +13,7 @@ public class NettyServer {
     private static final int BEGIN_PORT = 8000;
 
     public static void main(String[] args) {
+
         NioEventLoopGroup boosGroup = new NioEventLoopGroup();
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
@@ -21,21 +22,38 @@ public class NettyServer {
         serverBootstrap
                 .group(boosGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
+
+                //给NioServerSocketChannel指定一些自定义属性
                 .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+
+                //给每一条连接指定自定义属性
                 .childAttr(clientKey, "clientValue")
+
+                //系统用于临时存放已完成三次握手的请求的队列的最大长度
+                // 如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
                 .option(ChannelOption.SO_BACKLOG, 1024)
+
+                //表示是否开启TCP底层心跳机制，true为开启
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
+
+                //表示是否开启Nagle算法，true表示关闭，false表示开启
+                // 通俗地说，如果要求高实时性，有数据发送时就马上发送，就关闭，如果需要减少发送次数减少网络交互，就开启。
                 .childOption(ChannelOption.TCP_NODELAY, true)
+
+                //childHandler()用于指定处理新连接数据的读写处理逻辑
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
                         System.out.println(ch.attr(clientKey).get());
                     }
                 });
-
-
         bind(serverBootstrap, BEGIN_PORT);
     }
 
+    /**
+     * @Author sgl
+     * @Description 通过回调方法监听实现了服务端绑定端口，依次递增
+     * @Date 2020/04/29 下午 16:55
+     **/
     private static void bind(final ServerBootstrap serverBootstrap, final int port) {
         serverBootstrap.bind(port).addListener(future -> {
             if (future.isSuccess()) {
